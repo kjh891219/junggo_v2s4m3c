7f21db3d98b4de1a1c6789438b61c9548eb9a733
soldesk <soldesk@soldesk-pc-1-20>
2016-11-02 18:29:29 +0900

    yeon

--- src/main/webapp/js/event.js
+++ src/main/webapp/js/event.js
@@ -1,39 +1,43 @@
 $(document).ready(function(){
    
-   $("#menu_top").css('background-color','#3b78ce').css('color','white');
-   $("#menu_top").next().css('display','block');
-   
-   $(".left_list").mouseenter(function(){
-     $("#menu_top").css('background-color','transparent').css('color','black');
-     $("#menu_top").next().css('display','none');
-     $(this).css('background-color','#3b78ce').css('color','white');
-     $(this).next().css('display','block');
-     /* Toggle('on'); .delay(5000) */
-      $(".left_detail").mouseenter(function(){
-        $("#menu_top").css('background-color','transparent').css('color','black');
-        $("#menu_top").next().css('display','none');
-        $(this).prev().css('background-color','#3b78ce').css('color','white');
-        $(this).css('display','block');
+      
+      $("#left_list_f").css('background-color','#3b78ce').css('color','white');
+      $("#left_list_f").next().css('display','block');
+      
+      
+      $(".left_list").mouseenter(function(){
+        $("#left_list_f").css('background-color','transparent').css('color','black');
+        $("#left_list_f").next().css('display','none');
+        $(this).css('background-color','#3b78ce').css('color','white');
+        $(this).next().css('display','block');
+        /* Toggle('on'); .delay(5000) */
+         $(".left_detail").mouseenter(function(){
+            $("#left_list_f").css('background-color','transparent').css('color','black');
+            $("#left_list_f").next().css('display','none');
+           $(this).prev().css('background-color','#3b78ce').css('color','white');
+           $(this).css('display','block');
+               
+         });     
+         $(".left_detail").mouseleave(function(){
+            $(this).prev().css('background-color','transparent').css('color','black');
+            $(this).css('display','none');
+            $("#left_list_f").css('background-color','#3b78ce').css('color','white');
+            $("#left_list_f").next().css('display','block');
+         });     
+      });
+      
+      $(".left_list").mouseleave(function(){
+            
+            $(this).css('background-color','transparent').css('color','black');
+            $(this).next().css('display','none');
+            
+            $("#left_list_f").css('background-color','#3b78ce').css('color','white');
+            $("#left_list_f").next().css('display','block');
             
       });     
-      $(".left_detail").mouseleave(function(){
-         $(this).prev().css('background-color','transparent').css('color','black');
-         $(this).css('display','none');
-         $("#menu_top").css('background-color','#3b78ce').css('color','white');
-         $("#menu_top").next().css('display','block');
-      });     
-   });
-   
-   $(".left_list").mouseleave(function(){
          
-         $(this).css('background-color','transparent').css('color','black');
-         $(this).next().css('display','none');
-         $("#menu_top").css('background-color','#3b78ce').css('color','white');
-         $("#menu_top").next().css('display','block');
-         
-   });
-   
-   
+         /* Toggle('on'); .delay(5000) */
+      
       
 
 });
